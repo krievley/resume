b296424047ac182d3dee8df6a790b60606c0f9c0
Kristin Rievley <krievley@gmail.com>
2015-06-30 21:22:00 -0400

    Added assets

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Resume Home</title>
+        {!! HTML::style('css/normalize.css') !!}
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+            </div>
+        </div>
+    </body>
+</html>
